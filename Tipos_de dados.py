2fd608a121ac2896c38b463da8c4b9feac9dce2e
ArthurdePau <m.sheikprooficial@gmail.com>
2025-12-04 16:51:47 -0300

    Add files via upload

---
+++ Tipos_de dados.py
@@ -0,0 +1,56 @@
+# objetivo mostrar o que está dentro do parenteses dela no
+# no ambiente de teste
+# 4 tipos strings float inteiro, boleanos
+
+# quebra de linha em python
+print('Olá seja bem vindo Júlia, \n Obrigada volte sempre.',)
+print(" House's")
+# literais
+# concatenação - organizar
+print("nome","uva", "teste")
+print(1)
+print(5.0)
+print(3e8)
+print(6.62607e-37)
+
+print(True + 3)
+print(0 + False)
+
+# aritmético - sempre um númerocomo saida ***
+print(10 + 2) 
+print(10 - 2)
+print(10 / 2)
+print(10 ** 2)
+print(10 * 2) 
+print(10 % 2)
+print(10 // 2)
+print(16 ** 0.5)
+print()
+# lógicos - True or False
+
+print(10 > 2) # maior
+print(10 < 2) # menor
+print(10 >= 2) # maior ou igual
+print(10 <= 2) # menor ou igual
+print(10 == 2) # igual
+print(10 != 2) # diferente
+print()
+
+print(2**2**3)
+
+print()
+# estrutura de dado uma palavra =
+
+# fracamente tipadas
+# dinamicamente
+# únicas
+
+variavael = 5
+Variavel = 10
+VARIAVEL = 20
+print(VARIAVEL)
+print(variavael)
+print()
+# numeros não, espaçamento não,
+teste = 35
+
