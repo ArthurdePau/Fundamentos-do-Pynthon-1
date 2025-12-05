0b2e28cdea7460556705cbcfdafc4e2b94edcd7f
ArthurdePau <m.sheikprooficial@gmail.com>
2025-12-05 14:15:09 -0300

    Add files via upload

---
+++ Estrutura_dados.py
@@ -0,0 +1,27 @@
+a = 10
+a = 10 + 2
+a = a + 2
+a = a - 2
+a = a / 2
+a = a * 2
+a += 2
+
+a += 2
+a *= 2
+a /= 2
+a -= 2
+
+print()
+n1 = float(input( 'n: '))
+n2 = float(input( 'n: '))
+soma = n1 + n2
+print('=', soma)
+
+numero = 10
+print(str(numero)*10)
+
+# type casting float int bool str
+print(str(numero) )
+print(int(numero) )
+print(str(numero) )
+print(bool(numero) )
---
+++ atividade 1.py
@@ -0,0 +1,13 @@
+print(3e3)
+
+fruta_1 = 'banana'
+fruta_2 = 'laranja'
+fruta_3 = 'pinha'
+
+print(fruta_1, fruta_2, fruta_3, sep=',')
+print()
+n1 = int(input( 'n: '))
+n2 = int(input( 'n: '))
+print(n1, n2)
+print()
+python = "Pynthom é show"
