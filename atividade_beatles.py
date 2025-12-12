bafe3e42bd7948a7ca114c0bd933f6ebce4930ad
ArthurdePau <m.sheikprooficial@gmail.com>
2025-12-12 14:11:46 -0300

    Add files via upload

---
+++ atividade_beatles.py
@@ -0,0 +1,33 @@
+# etapa 1
+beatles = []
+print("Etapa 1:", beatles)
+
+
+# etapa 2
+
+print("Etapa 2:", beatles)
+beatles.append('John Lennon')
+beatles.append('Paul McCartney')
+beatles.append('George Harrison')
+
+# etapa 3
+
+print("Etapa 3:", beatles)
+membros = 'Stu Sutcliffe e Pete Best'
+
+while 
+    
+
+# etapa 4
+
+print("Etapa 4:", beatles)
+
+# passo 5
+
+print("Etapa 5:", beatles)
+
+
+
+# testando o tamanho da lista
+
+("o fabuloso", len(beatles))
